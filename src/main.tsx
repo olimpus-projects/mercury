73c3401c4ba20b167985bd47cc7190bff090d2bb
Samuel Santos <32316175+samuel-cezar@users.noreply.github.com>
2025-04-24 22:52:33 -0300

    ✨Implemented Home and Login pages with routing setup. Updated dependencies in package.json. 🚀

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Home from './routes/Home/Home'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Login from './routes/Login/Login'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <div className="hello-world">
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default Home
